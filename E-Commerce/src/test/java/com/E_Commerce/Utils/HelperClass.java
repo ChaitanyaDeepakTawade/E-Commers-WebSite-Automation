19ae4997bc936a969aaa395ef9d016cb43abb20a
Chaitanya Tawade <omtawade2002@gmail.com>
2025-07-13 01:26:53 +0530

    ss add , retry and multi browser

--- E-Commerce/src/test/java/com/E_Commerce/Utils/HelperClass.java
+++ E-Commerce/src/test/java/com/E_Commerce/Utils/HelperClass.java
@@ -10,35 +10,43 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class HelperClass {
 	
+	
 	private static HelperClass helperclass;
-	protected WebDriver driver;
+	protected static WebDriver driver;
 	public final static int TIMEOUT =5;
 	protected Properties prop;
 	DateTimeFormatter Date = DateTimeFormatter.ofPattern("ddmmyyyyHHmmss");
 	String timestamp = LocalDateTime.now().format(Date);
 	protected String uniqueEmail ="Ecomtest"+timestamp+"@gmail.com";
 	
-	
-
+	@Parameters("browser")
     @BeforeClass
-    public void setUp() throws Exception {
+    public void setUp(String browser) throws Exception {
         prop = new Properties();
         prop.load(new FileInputStream("src\\test\\ resources\\config.properties"));
 
-        String browser = prop.getProperty("browser");
+        //String browser = prop.getProperty("browser");
+        
         if (browser.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
+        }else if(browser.equalsIgnoreCase("edge")) {
+        	driver = new EdgeDriver();
         }
-
         driver.manage().window().maximize();
         driver.get(prop.getProperty("url"));
     }
-
+   
+    public static WebDriver getDriver() {
+    	
+    	return driver;
+    }
   // @AfterClass
     public void tearDown() {
         if (driver != null) {
--- E-Commerce/src/test/java/com/E_Commerce/listeners/listeners.java
+++ E-Commerce/src/test/java/com/E_Commerce/listeners/listeners.java
@@ -1,14 +1,27 @@
 package com.E_Commerce.listeners;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.E_Commerce.Utils.ExtentTestManager;
+import com.E_Commerce.Utils.HelperClass;
 
 public class listeners implements ITestListener {
-
+	
     @Override
     public void onStart(ITestContext context) {
         System.out.println("Test Suite Started: " + context.getName());
@@ -31,11 +44,17 @@ public class listeners implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
+    	
         ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + result.getThrowable());
+        String base64 = ((TakesScreenshot) HelperClass.getDriver()).getScreenshotAs(OutputType.BASE64);
+        ExtentTestManager.getTest().fail("Screenshot:",MediaEntityBuilder.createScreenCaptureFromBase64String(base64).build());
+
     }
 
-    @Override
+	@Override
     public void onTestSkipped(ITestResult result) {
         ExtentTestManager.getTest().log(Status.SKIP, "Test Skipped");
     }
+    
+ 
 }
